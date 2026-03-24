a79018de5d053fb0319c426bf2635646af42e371
Fgter <1651794787@qq.com>
2026-03-24 11:46:26 +0800

    删除测试宏

--- Assets/BuildTool/Editor/ScriptableObjectScripts/CustomScriptingDefineConfig.cs
+++ Assets/BuildTool/Editor/ScriptableObjectScripts/CustomScriptingDefineConfig.cs
@@ -12,9 +12,6 @@ namespace BuildTool
         public enum CustomScriptingDefine
         {
             ORBIT_GM = 1 << 0,
-            ORBIT_GMw = 1 << 1,
-            ORBIT_GMww = 1 << 2,
-            ORBIT_GMwww = 1 << 3,
         }
     }
 }
